f37cc9aa3f2a9d8c0a0e75ea7b1be3fba108c0d7
Videme Auguste Thémis MONTCHO <138248780+Vmontcho@users.noreply.github.com>
2024-06-25 17:13:11 +0000

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,12 @@
+// config.js
+const config = {
+  apiKey: 'patT8LU66ZKxgmAEn.c020268967e1b2e4c2d4830a5cdbfab5fcc20dd4531638eeb95150dc261938b6',
+  baseId: 'appJjYz4MFfU5giE6',
+  tables: {
+    products: 'tblEK9jSF4AgQb0pE',
+    categories: 'tblEK9jSF4AgQb0pE', // Mettez à jour avec le bon ID de la table pour les catégories
+    skus: 'tblEK9jSF4AgQb0pE' // Mettez à jour avec le bon ID de la table pour les SKUs
+  }
+};
+
+export default config;
