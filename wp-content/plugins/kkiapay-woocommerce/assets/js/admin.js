6727dc29c28eedf126619a79e010f2062a20a6cc
Videme Auguste Thémis MONTCHO <montchovideme@gmail.com>
2024-10-27 14:24:09 +0000

    v4.6

--- wp-content/plugins/kkiapay-woocommerce/assets/js/admin.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener("load",function () {
-    const color_input = document.querySelector("#woocommerce_kkiapay_woocommerce_plugin_theme");
-    if (color_input) color_input.className += "jscolor"
-
-});
