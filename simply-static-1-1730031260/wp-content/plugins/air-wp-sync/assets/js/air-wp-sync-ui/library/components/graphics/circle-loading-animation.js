615ccdab9af9f30b92a0d7e62bceca7e229b667b
Videme Auguste Thémis MONTCHO <montchovideme@gmail.com>
2024-10-27 14:23:08 +0000

    v4.5

--- simply-static-1-1730031260/wp-content/plugins/air-wp-sync/assets/js/air-wp-sync-ui/library/components/graphics/circle-loading-animation.js
+++
@@ -1,8 +0,0 @@
-import { jsx } from 'react/jsx-runtime';
-import SvgCircleLoading from './icons/CircleLoading.tsx.js';
-import 'react';
-
-const CircleLoadingAnimation = (props) => (jsx("div", { className: "airwpsync-c-circle-loading", children: jsx(SvgCircleLoading, { ...props }) }));
-
-export { CircleLoadingAnimation as default };
-//# sourceMappingURL=circle-loading-animation.js.map
